bf77ad185faaa671fb73978741213ba5eaf9e5c8
hardiksoni019 <hardiksoni019@gmail.com>
2026-07-26 14:03:10 +0000

    remove export from next config

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,6 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   allowedDevOrigins: ['3000-firebase-vote-me-1768205766339.cluster-a6zx3cwnb5hnuwbgyxmofxpkfe.cloudworkstations.dev'],
-  output: 'export',
   async headers() {
     return [
       {
